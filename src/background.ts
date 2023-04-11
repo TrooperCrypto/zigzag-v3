41ba34883abe7d57af786b1b1a58e3919f0fd7bf
TrooperCrypto <trooper.crypto7@gmail.com>
2023-04-11 23:13:41 +0200

    update on chain listeners

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,14 @@
 import fs from 'fs'
 import { ethers } from 'ethers'
 import { db, runDbMigration, removeExpiredOrders } from './db'
+import type { EventLogs } from './types'
 
-async function updateOrderFilled(orderHash: string, filled: string, ) {
+let iFace: ethers.utils.Interface
+
+async function updateOrderFilled(orderHash: string, filled: string) {
   console.log(`New swap! orderHash: ${orderHash}, filled: ${filled}`)
-  await db.query(`
+  await db.query(
+    `
     UPDATE
       orders
     SET
@@ -18,7 +22,8 @@ async function updateOrderFilled(orderHash: string, filled: string, ) {
 
 async function updateOrderCanceled(orderHash: string) {
   console.log(`Order canceld. orderHash: ${orderHash}`)
-  await db.query(`
+  await db.query(
+    `
     DELETE FROM
       orders
     WHERE
@@ -28,52 +33,116 @@ async function updateOrderCanceled(orderHash: string) {
   )
 }
 
-async function startListeners(contract: ethers.Contract) {
-  contract.on(
-    'OrderStatus',
-    (
-      orderHash: string,
-      filled: ethers.BigNumber,
-      remaining: ethers.BigNumber,
-      blockData: any
-    ) => {
-      updateOrderFilled(orderHash, filled.toString())
-        .catch((err: any) => {
-          console.log(`Failed to handle 'OrderStatus' ${err}`)
-          console.log(err)
-        })
+async function getFilterId(
+  provider: ethers.providers.JsonRpcProvider,
+  chainId: number,
+  exchangeAddress: string
+): Promise<string> {
+  let newFilter: string | undefined
+  while (newFilter == undefined) {
+    try {
+      const latestBlocknumber: number = await provider.getBlockNumber()
+      const fromBlock: number = latestBlocknumber - 4_999
+      newFilter = await provider.send('eth_newFilter', [
+        {
+          fromBlock: `0x${fromBlock.toString(16)}`,
+          address: [exchangeAddress],
+          topics: [
+            [
+              iFace.getEventTopic('OrderStatus'),
+              iFace.getEventTopic('CancelOrder'),
+            ],
+          ],
+        },
+      ])
+    } catch (err: any) {
+      console.log(`Failed to setup new filter for ${chainId} - ${err.body}`)
+
+      // wait for node to reconnect
+      await new Promise((resolve) => setTimeout(resolve, 1000))
     }
+  }
+
+  return newFilter
+}
+
+async function fetchNewLogs(
+  provider: ethers.providers.JsonRpcProvider,
+  chainId: number,
+  filterId: string
+) {
+  let logs: EventLogs[] = []
+  try {
+    logs = await provider.send('eth_getFilterChanges', [filterId])
+  } catch (err: any) {
+    throw new Error(
+      `Error sending eth_getFilterChanges for ${chainId}, updating filter - ${err.body}`
+    )
+  }
+
+  const promise = logs.map(async (log: EventLogs) => {
+    try {
+      if (log.removed) return
+
+      const parsedData = iFace.parseLog({
+        topics: log.topics,
+        data: log.data,
+      })
+
+      if (parsedData.name === 'OrderStatus') {
+        const [orderHash] = parsedData.args as [string]
+        await updateOrderCanceled(orderHash)
+      } else if (parsedData.name === 'CancelOrder') {
+        const [orderHash, filled, remaining] = parsedData.args as [
+          string,
+          ethers.BigNumber,
+          ethers.BigNumber
+        ]
+
+        await updateOrderFilled(orderHash, filled.toString())
+      }
+    } catch (err: any) {
+      throw new Error(`Error handling new logs for ${chainId} - ${err}`)
+    }
+  })
+  await Promise.all(promise)
+}
+
+async function startFilters(chainId: number, exchangeAddress: string) {
+  const provider = new ethers.providers.JsonRpcBatchProvider(
+    process.env.RPC_ARBITRUM
   )
 
-  contract.on(
-    'CancelOrder',
-    (
-      orderHash: string,
-      blockData: any
-    ) => {
-      updateOrderCanceled(orderHash)
-        .catch((err: any) => {
-          console.log(`Failed to handle 'Swap' ${err}`)
-          console.log(err)
-        })
+  let filterId: string = await getFilterId(provider, chainId, exchangeAddress)
+
+  setInterval(async () => {
+    try {
+      await fetchNewLogs(provider, chainId, filterId)
+    } catch (err: any) {
+      console.warn(err.message)
+      filterId = await getFilterId(provider, chainId, exchangeAddress)
     }
-  )
-  
-  console.log('background.ts: Started listeners')
+  }, 500)
 }
 
 async function start() {
   console.log('background.ts: Starting')
   await runDbMigration()
 
-  const exchangeAbi = JSON.parse(fs.readFileSync('abi/ZigZagExchange.json', 'utf8')).abi
   const EVMConfig = JSON.parse(fs.readFileSync('EVMConfig.json', 'utf8'))
+  const exchangeAbi = JSON.parse(
+    fs.readFileSync('abi/ZigZagExchange.json', 'utf8')
+  ).abi
+  iFace = new ethers.utils.Interface(exchangeAbi)
 
-  const provider = new ethers.providers.JsonRpcBatchProvider(process.env.INFURA_ARBITRUM)
-  const contract = new ethers.Contract(EVMConfig.onChainSettings.exchangeAddress, exchangeAbi, provider)
-  await startListeners(contract)
+  try {
+    await startFilters(42161, EVMConfig.onChainSettings.exchangeAddress)
+    await startFilters(42161, EVMConfig.onChainSettings.exchangeAddress)
+  } catch (err: any) {
+    console.error(`background - Failed to set up event listener`, err)
+  }
 
-  setInterval(removeExpiredOrders, 2 * 1000)
+  setInterval(removeExpiredOrders, 500)
 }
 
 start()
